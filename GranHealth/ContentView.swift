a8c1c8a9b1a515cbe0c2e64f689f5ab54461c30c
Siddharth1010 <siddharthmani2000@gmail.com>
2021-02-17 10:12:11 +0530

    Initial Commit

---
+++ GranHealth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GranHealth
+//
+//  Created by MANI NAIR on 17/02/21.
+//  Copyright © 2021 com.siddharthnair. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
