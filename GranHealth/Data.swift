1cc15ee689638c0b1bb8e77084bb6f0a76e648b7
Siddharth1010 <siddharthmani2000@gmail.com>
2021-06-27 18:15:22 +0530

    Update LocationManager

--- GranHealth/Data.swift
+++ GranHealth/Data.swift
@@ -8,17 +8,19 @@ let healthData:[Health] = load("health.json")
 func load<T: Decodable>(_ filename:String, as type:T.Type = T.self) -> T {
     
     let data:Data
+    // Search for file in main bundle
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
         else{
             fatalError("Couldnt find \(filename) in main bundle")
     }
-    
+    // Extract contents of JSON file
     do{
         data = try Data(contentsOf: file)
     } catch {
         fatalError("Couldnt load \(filename) from main bundle:\n\(error)")
     }
     
+    // Decode JSON and return to array of Health
     do{
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
--- GranHealth/LocationManager.swift
+++ GranHealth/LocationManager.swift
@@ -1,19 +1,17 @@
-//
-//  LocationManager.swift
-//  GranHealth
-//
-//  Created by MANI NAIR on 25/05/21.
-//  Copyright © 2021 com.siddharthnair. All rights reserved.
-//
 
 import Foundation
 import MapKit
 
+// Create LocationManager class conforming to ObservableObject
 class LocationManager: NSObject, ObservableObject {
     
     private let locationManager = CLLocationManager()
+    
+    // Published var to ensure reflection in changes
     @Published var location: CLLocation? = nil
     
+    // Override to provide the same initializer that the parent class already has
+    // Get best accuracy, Background location updates and Start updating location
     override init() {
         super.init()
         self.locationManager.delegate = self
@@ -25,10 +23,12 @@ class LocationManager: NSObject, ObservableObject {
     }
 }
 
+// Extension of LocationManager conforming to CLLocationManagerDelegate
 extension LocationManager : CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
+        // Get the last location and initialise self.location with the corresponding location
         guard let location = locations.last else{
             return
         }
